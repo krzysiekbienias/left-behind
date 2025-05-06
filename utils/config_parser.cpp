05c403e347140df436df213b9252dadce2f81114
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2025-05-06 22:56:30 +0200

    feat(config): add support for STANDARISED_INPUT key in config loader.

--- utils/config_parser.cpp
+++ utils/config_parser.cpp
@@ -9,6 +9,7 @@ Config Config::from_json(const std::string& path) {
 
     Config config;
     std::vector<std::string> keys = {
+        "STANDARISED_INPUT",
         "STUDENTS_DATA",
         "ALPHA_VENTAGE_API_KEY",
         "FRED_API_KEY",
