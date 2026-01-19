0696d98377f2ae167224a983a0d1ba20f4ba212b
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-19 10:40:56 +0100

    fix(string utils):  fix typos and unify names

--- utils/string_utils.cpp
+++ utils/string_utils.cpp
@@ -4,7 +4,7 @@
 #include <cctype> //for ::isdigit
 
 
-namespace StringUtils {
+namespace string_utils {
 bool isAllDigits(const std::string& str){
     return !str.empty() && std::all_of(str.begin(), str.end(), ::isdigit);
 }
--- utils/string_utils.hpp
+++ utils/string_utils.hpp
@@ -2,6 +2,18 @@
 #include<string>
 
 
-namespace StringUtils{
-bool isAllDigits(const std::string& str);
+namespace string_utils{
+    bool isAllDigits(const std::string& str);
+
+
+    inline std::string trim(const std::string& s) {
+        size_t b = 0;
+        size_t e = s.size();
+
+        while (b < e && std::isspace(static_cast<unsigned char>(s[b]))) ++b;
+        while (e > b && std::isspace(static_cast<unsigned char>(s[e - 1]))) --e;
+
+        return s.substr(b, e - b);
+    }
+
 }
