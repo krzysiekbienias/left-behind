fc3b4c8d350ef5b4b5747b5e13ef09ceb65ad192
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-21 17:35:04 +0100

    feat(Potion): add static validator to volume that prevents invalid constructor.

--- src/potion.cpp
+++ src/potion.cpp
@@ -1,6 +1,12 @@
 #include<array>
 #include <potion.hpp>
 
+static unsigned int validateVolume(unsigned int v) {
+    if (v==0)  {
+        throw std::invalid_argument("Potion::validateVolume : Volume must be positive");
+    }
+    return v;
+}
 
-Potion::Potion(const std::array<unsigned char,3>& color,unsigned int volume):color(color),volume(volume){};
+Potion::Potion(const std::array<unsigned char,3>& color,unsigned int volume):m_color(color),m_volume(validateVolume(volume)) {};
 
