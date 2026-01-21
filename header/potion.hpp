7b18bc05691b5632c3b75fbfecd4f0ecc8991c8c
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-21 22:20:21 +0100

    feat(Potion): implement colorToString to represent poition's colorm represented by sting rbg.
    Implement method to mix two potions and produce new mixture.

--- header/potion.hpp
+++ header/potion.hpp
@@ -1,13 +1,16 @@
 #pragma once
 #include<array>
 
+using uch = unsigned char;
+std::string colorToString(std::array<uch,3>col);
+
 class Potion {
 public:
-    std::array<unsigned char,3> color; //unsigned char [0,255]
-    unsigned int volume;
+    std::array<unsigned char,3> m_color; //unsigned char [0,255]
+    unsigned int m_volume;
 
     Potion(const std::array<unsigned char,3>& color,unsigned int volume);
 
-    Potion mix(Potion &other);
+    Potion mix(Potion &other) const;
 
 };
--- src/potion.cpp
+++ src/potion.cpp
@@ -1,12 +1,42 @@
 #include<array>
 #include <potion.hpp>
+#include <string>
+#include <ostream>
+#include <sstream>
+#include <strstream>
+
+using ui = unsigned int;
+using uch = unsigned char;
+using u64 = std::uint64_t;
+
 
 static unsigned int validateVolume(unsigned int v) {
-    if (v==0)  {
+    if (v == 0) {
         throw std::invalid_argument("Potion::validateVolume : Volume must be positive");
     }
     return v;
 }
 
-Potion::Potion(const std::array<unsigned char,3>& color,unsigned int volume):m_color(color),m_volume(validateVolume(volume)) {};
+std::string colorToString(std::array<uch,3>col) {
+    std::ostringstream os;
+    os<<"{R: "<<(int)col[0]<<", "<<"G: "<<(int)col[1]<<", "<<"B: "<<(int)col[2]<<"}";
+    return os.str();
+}
 
+Potion::Potion(const std::array<unsigned char, 3> &color, unsigned int volume) : m_color(color),
+    m_volume(validateVolume(volume)) {
+};
+
+
+Potion Potion::mix(Potion &other)  const{
+    //add on bigger range
+    u64 r = (this->m_color[0] * this->m_volume + other.m_color[0] * other.m_volume);
+    u64 g = (this->m_color[1] * this->m_volume + other.m_color[1] * other.m_volume);
+    u64 b = (this->m_color[2] * this->m_volume + other.m_color[2] * other.m_volume);
+    ui combinedVolume = this->m_volume + other.m_volume;
+    uch red = static_cast<uch>(std::ceil(static_cast<double>(r) / combinedVolume));
+    uch green = static_cast<uch>(std::ceil(static_cast<double>(g) / combinedVolume));
+    uch blue = static_cast<uch>(std::ceil(static_cast<double>(b) / combinedVolume));
+
+    return Potion({red, green, blue}, combinedVolume);
+}
