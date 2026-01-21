3cb563dea9e6406dcbc2a685b31e8ade1457b280
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-01-21 22:17:51 +0100

    test(Potion): add test to mix two positions.

--- unit_tests/test_potion.cpp
+++ unit_tests/test_potion.cpp
@@ -20,8 +20,28 @@ TEST(Potion, ConstructorWith0Volume)
     std::array<unsigned char, 3> color{255, 128, 0};
     unsigned int volume = 0u;
 
-
     EXPECT_THROW(Potion(color,volume),std::exception);
+
+
+}
+
+
+TEST(Potion, Mix2Eliksirs)
+{
+    std::array<unsigned char, 3> color1{255, 255, 255};
+    unsigned int volume1 = 7;
+    std::array<unsigned char, 3> color2{51, 102, 51};
+    unsigned int volume2 = 12;
+
+    Potion p(color1, volume1);
+    Potion p2(color2, volume2);
+
+    Potion p3= p.mix(p2);
+
+    EXPECT_EQ(colorToString(p3.m_color),"{R: 127, G: 159, B: 127}");
+
+
+
 
 
 }
