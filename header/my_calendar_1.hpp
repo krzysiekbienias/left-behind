b084343e447daf99a70f89ba659d7596b9f7c08f
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-04-22 16:14:24 +0200

    feat(design): optimize MyCalendar booking logic by simplifying boundary checks and removing debug output

--- header/my_calendar_1.hpp
+++ header/my_calendar_1.hpp
@@ -7,6 +7,7 @@ public:
     MyCalendar() {
 
     }
+    //represents start (key) end (value) for each event
     std::map<int,int> calendar;
 
     bool book(int startTime, int endTime);
--- src/my_calendar_1.cpp
+++ src/my_calendar_1.cpp
@@ -8,35 +8,22 @@ using namespace io;
 
 
 bool MyCalendar:: book(int startTime, int endTime) {
-    cout <<"Incoming event"<<endl;
-    cout<<"\t[" <<startTime<<","<<endTime<<")"<<endl;
-    if (calendar.empty()) {
+    auto nextIt=calendar.lower_bound(startTime); //lower_bound from definition gives us next key >= startTime
+    // -->
+    if (nextIt!=calendar.end()) {
+        int nextStart=nextIt->first;
+        int nextEnd=nextIt->second;
 
-        calendar[startTime]=endTime;
-        return true;
+        if (endTime>nextStart) return false;
     }
+        // <--
+        if (nextIt!=calendar.begin()){
+            auto prevIt=std::prev(nextIt);
+            int prevStart=prevIt->first;
+            int prevEnd=prevIt->second;
+            if (startTime<prevEnd) return false;
 
-    auto it=calendar.lower_bound(startTime);
-
-    if (it!=calendar.end() && endTime>it->first) {
-        std::cout<<"next: ["<<it->first<< ","<<it->second<<")\n";
-
-        return false;
-    }
-    else {
-        cout<<"there is no next \n";
-    }
-
-    if (it!=calendar.begin()) {
-        auto pit=std::prev(it);
-        std::cout<<"prev: ["<<pit->first<<","<<pit->second<<")\n";
-        if (startTime<pit->second) return false;
-    }
-    else cout <<"there is no previous event"<<endl;
-
+        }
     calendar.emplace(startTime,endTime);
     return true;
-
-    return true;
-
-}
+    }
