fb58434efc41485d0c09c07afdf29230c370f146
krzysiekbienias <krzysiek.bienias.kr@gmail.com>
2026-04-22 16:08:38 +0200

    feat(design): implement LRU Cache with deque and unordered map, and add comprehensive unit tests

---
+++ header/lru_cache.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <deque>
+#include <unordered_map>
+
+
+class LRUCache {
+public:
+    LRUCache(int capacity);
+    int get(int key);
+    void put(int key, int value);
+private:
+    int m_capacity;
+    std::deque<int> m_lru; // in m_lru we store keys
+    std::unordered_map<int,int> m_kv;
+};
+
